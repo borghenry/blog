ba8a9f246e150b97f7b3b9cc40c3f119722b0a09
borghenry <borghenry@gmail.com>
2018-05-20 11:46:54 +0100

    add short cv

--- R/rendercv.R
+++ R/rendercv.R
@@ -5,3 +5,8 @@ rmarkdown::render(here('R', 'eborghettocv.Rmd'),
                   output_format = 'all', 
                   output_dir = here("static", "cv"), 
                   encoding = "UTF-8")
+
+rmarkdown::render(here('R', 'eborghettocv_short.Rmd'), 
+                  output_format = 'all', 
+                  output_dir = here("static", "cv"), 
+                  encoding = "UTF-8")
