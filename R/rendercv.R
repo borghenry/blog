208040f8e217d423fd8f78b451fe5e1edd84912f
borghenry <borghenry@gmail.com>
2018-07-25 14:35:59 +0100

    added risp 2018 to cv

--- R/rendercv.R
+++ R/rendercv.R
@@ -5,8 +5,3 @@ rmarkdown::render(here('R', 'eborghettocv.Rmd'),
                   output_format = 'all', 
                   output_dir = here("static", "cv"), 
                   encoding = "UTF-8")
-
-rmarkdown::render(here('R', 'eborghettocv_short.Rmd'), 
-                  output_format = 'all', 
-                  output_dir = here("static", "cv"), 
-                  encoding = "UTF-8")
