1df2196fb11e2167c5a29b568f3924b527f0cfbe
borghenry <borghenry@gmail.com>
2018-11-15 17:29:31 +0000

    update CV

--- R/rendercv.R
+++ R/rendercv.R
@@ -2,6 +2,11 @@ library(rmarkdown)
 library(here)
 
 rmarkdown::render(here('R', 'eborghettocv.Rmd'), 
-                  output_format = 'all', 
+                  output_format = c("html_document", "pdf_document", "word_document"),
+                  output_dir = here("static", "cv"), 
+                  encoding = "UTF-8")
+
+rmarkdown::render(here('R', 'eborghettocv.Rmd'), 
+                  output_format = "pdf_document",
                   output_dir = here("static", "cv"), 
                   encoding = "UTF-8")
