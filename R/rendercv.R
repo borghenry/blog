f621ea545b72582dcf08942d68319ca6b142b34e
borghenry <borghenry@gmail.com>
2019-09-20 16:02:19 +0100

    updated CV

--- R/rendercv.R
+++ R/rendercv.R
@@ -7,6 +7,17 @@ rmarkdown::render(here('R', 'eborghettocv.Rmd'),
                   encoding = "UTF-8")
 
 rmarkdown::render(here('R', 'eborghettocv.Rmd'), 
-                  output_format = "html_document",
+                  output_format = c("html_document", "word_document"),
                   output_dir = here("static", "cv"), 
                   encoding = "UTF-8")
+
+rmarkdown::render(here('R', 'eborghettocv_ita.Rmd'), 
+                  output_format = "pdf_document",
+                  output_dir = here("static", "cv"), 
+                  encoding = "UTF-8")
+
+rmarkdown::render(here::here('R', 'eborghettocv_ita.Rmd'), 
+                  output_format = c("html_document", "word_document"),
+                  output_dir = here::here("static", "cv"), 
+                  encoding = "UTF-8")
+ 
